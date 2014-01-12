657c154f3262f1c8e11451769a9b822de7701891
Xuncheng Wang <w.xun.cheng@gmail.com>
2014-01-12 17:32:29 +0800

    working follow feature with ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,19 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find_by_slug(params[:followed_id])
     current_user.follow!(@user) if current_user.can_follow?(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    relationship = Relationship.find(params[:id])
+    @user = Relationship.find(params[:id]).followed
+    relationship = Relationship.find_by(followed_id: @user)
     relationship.destroy if relationship.follower == current_user
-    redirect_to relationship.followed
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
